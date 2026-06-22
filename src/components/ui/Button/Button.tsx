6b19ec65b8d0f769733cb8e89f351217f1cbe338
Guiomar Pérez Montesdeoca <manquiito@gmail.com>
2026-06-22 14:13:09 +0100

    button for the whole website, with main and second button CTA

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,22 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import styles from "./Button.module.css";
+
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: "primary" | "secondary";
+  children: ReactNode;
+};
+
+export default function Button({
+  variant = "primary",
+  children,
+  ...props
+}: Props) {
+  return (
+    <button
+      {...props}
+      className={`${styles.button} ${styles[variant]}`}
+    >
+      {children}
+    </button>
+  );
+}
