3545a513f938a196942ba84e1c20d4ccdc0d76de
Guiomar Pérez Montesdeoca <manquiito@gmail.com>
2026-07-20 18:19:04 +0100

    GET method for contacts

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,10 +1,17 @@
-import { createContact } from "@/services/contact.service"
+import { createContact, getContacts } from "@/services/contact.service"
 
 export async function POST (req: Request){
-    console.log("Entró al endpoint");
-
   const body = await req.json()
-  console.log(body)
   const result = await createContact(body)
   return Response.json(result)
+}
+
+export async function GET (){
+  const result = await getContacts()
+
+  return Response.json({
+    ok: true,
+    contact: result
+  })
+
 }
